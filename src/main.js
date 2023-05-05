8c3235cec7d52b4f03f4a0c468a80f3dcd81c8d1
xBBBoo <2697751592@qq.com>
2023-05-05 21:36:03 +0800

    项目初始化-引入 Vant 组件库

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 // 导入路由实例对象
 import router from './router'
+
 // 导入 vuex 数据状态管理库
 import store from './store'
+
 // 导入全局样式文件
 import '@/styles/index.less'
+
+// 导入字体图标样式文件
+import '@/styles/icon.less'
+
+// 导入 Vant 核心组件库
+import Vant from 'vant'
+// 导入 Vant 样式表文件
+import 'vant/lib/index.css'
+
+// 将 Vant 组件库安装为 Vue 的插件
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
